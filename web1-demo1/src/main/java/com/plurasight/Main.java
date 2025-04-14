991405571d9665b1895c3eeb0b55168f4414013e
Ahmed Elhamzawy <aelhamzawy2020@gmail.com>
2025-04-14 10:54:18 -0800

    creating a variables and concat them

--- web1-demo1/src/main/java/com/plurasight/Main.java
+++ web1-demo1/src/main/java/com/plurasight/Main.java
@@ -2,6 +2,10 @@ package com.plurasight;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String word1 = "Hello";
+        String word2 = "World";
+        String Greeting;
+        Greeting = word1+" "+ word2;
+        System.out.println(Greeting);
     }
 }
